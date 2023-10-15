e59e485b58322f593693aadb27e62fbb9ff245db
SDY8023 <18438609381@163.com>
2023-10-15 14:38:46 +0800

    java学习12

---
+++ SparkStudy/src/main/scala/CaseClasses/ParametricData.scala
@@ -0,0 +1,13 @@
+package CaseClasses
+
+/**
+ * @ClassNmae: ParametricData
+ * @Auther: SDY
+ * @Description:
+ * @Date: 2023/10/15 10:05
+ * */
+case class ParametricData(id:Int, testGuid:String, testTxt:String, testNum:String,
+                          testType:String, siteNum:String, queueId:String, partId:String,
+                          pinIndex:String, pinValue:String, testIndex:String, testValue:String,
+                          transTestValue:String, testResult:String, createTime:Long, testSuite:String,
+                          vectorName:String, vectorPattern:String)
